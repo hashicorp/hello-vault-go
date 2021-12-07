6770e6483ec617beaea5fcfc4338ba2d423d1bb1
VinnyHC <vinny.mannello@hashicorp.com>
2021-12-07 08:35:45 -0800

    catch up

--- vault.go
+++ vault.go
@@ -4,22 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 
 	vault "github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/api/auth/approle"
 )
 
 type Vault struct {
 	client                  *vault.Client
-	role 					string
 	auth                    vault.AuthMethod
 	databaseCredentialsPath string
 	apiKeyPath              string
 }
 
 // NewVaultAppRoleClient returns a new client for interacting with Vault KVv2 secrets via AppRole authentication
-func NewVaultAppRoleClient(address, approleRoleIDFile, approleSecretIDFile, databaseCredentialsPath, apiKeyPath string) (*Vault, error) {
+func NewVaultAppRoleClient(address, approleRoleID, approleSecretIDFile, databaseCredentialsPath, apiKeyPath string) (*Vault, error) {
 	config := vault.DefaultConfig() // modify for more granular configuration
 	config.Address = address
 
@@ -28,21 +27,45 @@ func NewVaultAppRoleClient(address, approleRoleIDFile, approleSecretIDFile, data
 		return nil, fmt.Errorf("unable to initialize Vault client: %w", err)
 	}
 
-	role, err := ioutil.ReadFile(approleRoleIDFile)
+	// A combination of a Role ID and a Secret ID is required to log into Vault
+	// with AppRole authentication method. The Secret ID is a value that needs
+	// to be protected, so instead of the app having knowledge of the secret ID
+	// directly, we have a trusted orchestrator
+	// (https://learn.hashicorp.com/tutorials/vault/secure-introduction?in=vault/app-integration#trusted-orchestrator)
+	// give the app accev to a short-lived response-wrapping token
+	// (https://www.vaultproject.io/docs/concepts/response-wrapping).
+	// Read more at: https://learn.hashicorp.com/tutorials/vault/approle-best-practices?in=vault/auth-methods#secretid-delivery-best-practices
+	approleSecretID := &approle.SecretID{
+		FromFile: approleSecretIDFile,
+	}
+
+	appRoleAuth, err := approle.NewAppRoleAuth(
+		approleRoleID,
+		approleSecretID,
+		approle.WithWrappingToken(), // Only required if the secret ID is response-wrapped.
+	)
 	if err != nil {
-		return nil, fmt.Errorf("unable to retrieve Vault role: %w", err)
+		return nil, fmt.Errorf("unable to initialize approle authentication method: %w", err)
 	}
 
 	return &Vault{
 		client:                  client,
-		role:					 string(role),
+		auth:                    appRoleAuth,
 		databaseCredentialsPath: databaseCredentialsPath,
 		apiKeyPath:              apiKeyPath,
 	}, nil
 }
 
-// GetSecretAPIKey fetches the latest version of secret api key from kv-v2
+// GetSecret fetches the latest version of secret api key from kv-v2
 func (v *Vault) GetSecretAPIKey(ctx context.Context) (map[string]interface{}, error) {
+	authInfo, err := v.client.Auth().Login(ctx, v.auth)
+	if err != nil {
+		return nil, fmt.Errorf("unable to login using approle auth method: %w", err)
+	}
+	if authInfo == nil {
+		return nil, fmt.Errorf("no approle info was returned after login")
+	}
+
 	secret, err := v.client.Logical().Read(v.apiKeyPath)
 	if err != nil {
 		return nil, fmt.Errorf("unable to read secret: %w", err)
@@ -65,6 +88,15 @@ type DatabaseCredentials struct {
 
 // GetDatabaseCredentials retrieves a new set of temporary database credentials from Vault
 func (v *Vault) GetDatabaseCredentials(ctx context.Context) (DatabaseCredentials, error) {
+	// TODO: move all of these Logins to NewVaultAppRoleClient and kick off goroutine with LifetimeWatcher
+	authInfo, err := v.client.Auth().Login(ctx, v.auth)
+	if err != nil {
+		return DatabaseCredentials{}, fmt.Errorf("unable to login using approle auth method: %w", err)
+	}
+	if authInfo == nil {
+		return DatabaseCredentials{}, fmt.Errorf("no approle info was returned after login")
+	}
+
 	secret, err := v.client.Logical().Read(v.databaseCredentialsPath)
 	if err != nil {
 		return DatabaseCredentials{}, fmt.Errorf("unable to read secret: %w", err)
@@ -88,9 +120,18 @@ func (v *Vault) GetDatabaseCredentials(ctx context.Context) (DatabaseCredentials
 
 // PutSecret creates or overwrites a key-value secret (kv-v2) after authenticating via AppRole
 func (v *Vault) PutSecret(ctx context.Context, path string, data map[string]interface{}) error {
+	authInfo, err := v.client.Auth().Login(ctx, v.auth)
+	if err != nil {
+		return fmt.Errorf("unable to login using approle auth method: %w", err)
+	}
+
+	if authInfo == nil {
+		return fmt.Errorf("no approle info was returned after login")
+	}
+
 	data = map[string]interface{}{"data": data}
 
-	_, err := v.client.Logical().Write(path, data)
+	_, err = v.client.Logical().Write(path, data)
 	if err != nil {
 		return fmt.Errorf("unable to write secret: %w", err)
 	}
