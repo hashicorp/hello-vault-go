43f0a1a82a711f9099d029e21b7260f9b832c0e0
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-01-06 12:29:15 -0500

    Fix database renew/timeout issue

--- setup/vault-server/entrypoint.sh
+++ setup/vault-server/entrypoint.sh
@@ -12,12 +12,12 @@ set -e
 export VAULT_ADDR='http://127.0.0.1:8200'
 export VAULT_FORMAT='json'
 
-# spawn a new process for the development vault server and wait for it to come online
+# Spawn a new process for the development Vault server and wait for it to come online
 # ref: https://www.vaultproject.io/docs/concepts/dev-server
 vault server -dev -dev-listen-address="0.0.0.0:8200" &
 sleep 5s
 
-# authenticate container's local vault cli
+# Authenticate container's local Vault cli
 # ref: https://www.vaultproject.io/docs/commands/login
 vault login -no-print "${VAULT_DEV_ROOT_TOKEN_ID}"
 
@@ -25,7 +25,7 @@ vault login -no-print "${VAULT_DEV_ROOT_TOKEN_ID}"
 ########## ACCESS POLICIES ##########
 #####################################
 
-# add policies for the various roles we'll be using
+# Add policies for the various roles we'll be using
 # ref: https://www.vaultproject.io/docs/concepts/policies
 vault policy write trusted-orchestrator-policy /vault/config/trusted-orchestrator-policy.hcl
 vault policy write dev-policy /vault/config/dev-policy.hcl
@@ -34,28 +34,29 @@ vault policy write dev-policy /vault/config/dev-policy.hcl
 ######## APPROLE AUTH METHDO ########
 #####################################
 
-# enable AppRole auth method utilized by our web application
+# Enable AppRole auth method utilized by our web application
 # ref: https://www.vaultproject.io/docs/auth/approle
 vault auth enable approle
 
-# configure a specific AppRole role with associated parameters
+# Configure a specific AppRole role with associated parameters. We use
+# artificially low TTL values to demonstrate the renewal logic.
 # ref: https://www.vaultproject.io/api/auth/approle#parameters
 vault write auth/approle/role/dev-role \
     token_policies=dev-policy \
     secret_id_ttl="2m" \
     token_ttl="2m" \
-    token_max_ttl="6m"  # artificially low ttl to demonstrate token renewal
+    token_max_ttl="6m"
 
-# overwrite our RoleID with a known value to simplify our demo
+# Overwrite our RoleID with a known value to simplify our demo
 vault write auth/approle/role/dev-role/role-id role_id="${APPROLE_ROLE_ID}"
 
 #####################################
 ######### TOKEN AUTH METHOD #########
 #####################################
 
-# configure a token with permissions to act as a trusted orchestrator
-# nb: for simplicity, we don't handle renewals in our simulated orchestrator
-# so we've set the ttl to a very long duration (768h); when this expires
+# Configure a token with permissions to act as a trusted orchestrator.
+# For simplicity, we don't handle renewals in our simulated orchestrator
+# so we've set the TTL to a very long duration (768h). When this expires
 # the web app will no longer receive a SecretID and subsequently fail on the
 # next attempted AppRole login
 # ref: https://www.vaultproject.io/docs/commands/token/create
@@ -68,22 +69,22 @@ vault token create \
 ########## STATIC SECRETS ###########
 #####################################
 
-# enable a kv-v2 secrets engine, passing in the path parameter
+# Enable a kv-v2 secrets engine, passing in the path parameter
 # ref: https://www.vaultproject.io/docs/secrets/kv/kv-v2
 vault secrets enable -path=kv-v2 kv-v2
 
-# seed the kv-v2 store with an entry our web app will use
+# Seed the kv-v2 store with an entry our web app will use
 vault kv put "${API_KEY_PATH}" "${API_KEY_FIELD}=my-secret-key"
 
 #####################################
 ########## DYNAMIC SECRETS ##########
 #####################################
 
-# enable a database secrets engine
+# Enable a database secrets engine
 # ref: https://www.vaultproject.io/docs/secrets/databases
 vault secrets enable database
 
-# configure Vault's connection to our db, in this case PostgreSQL
+# Configure Vault's connection to our db, in this case PostgreSQL
 # ref: https://www.vaultproject.io/api/secret/databases/postgresql
 vault write database/config/my-postgresql-database \
     plugin_name=postgresql-database-plugin \
@@ -92,18 +93,21 @@ vault write database/config/my-postgresql-database \
     username="vault_db_user" \
     password="vault_db_password"
 
-# rotate the password for 'vault_db_user', ensures the user is only accessible by Vault itself
+# Rotate the password for 'vault_db_user', ensures the user is only accessible by Vault itself
 vault write -force database/config/my-postgresql-database
 
-# allow Vault to create roles dynamically with the same privileges as the 'readonly' role created in our database's init scripts
+# Allow Vault to create roles dynamically with the same privileges as the
+# 'readonly' role created in our database's init scripts. We use artificially
+# low TTL values to demonstrate the renewal logic.
 vault write database/roles/dev-readonly \
     db_name=my-postgresql-database \
     creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT readonly TO \"{{name}}\";" \
-    default_ttl="3m" \
-    max_ttl="7m"  # artificially low to demonstrate credential renewal logic
+    renew_statements="ALTER ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT readonly TO \"{{name}}\";" \
+    default_ttl="1m" \
+    max_ttl="3m"
 
-# this container is now healthy
+# This container is now healthy
 touch /tmp/healthy
 
-# keep container alive
+# Keep container alive
 tail -f /dev/null & trap 'kill %1' TERM ; wait
